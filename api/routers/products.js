6d8877e7c531a7469726961caea797dd82d95d30
Conny1 <joelconrad277@gmail.com>
2023-08-18 20:40:15 +0300

    error

--- api/routers/products.js
+++ api/routers/products.js
@@ -1,36 +1,36 @@
 import express from "express";
-import { addProduct, deleteProduct, getallOnCartegory, getallProduct, getProduct, searchProduct, updateProduct } from "../controlers/products.js";
-import { removeFromcart } from "../controlers/user.js";
+import {
+  addProduct,
+  deleteProduct,
+  getallOnCartegory,
+  getallProduct,
+  getProduct,
+  searchProduct,
+  updateProduct,
+} from "../controlers/products.js";
+// import { removeFromcart } from "../controlers/user.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
-
-const router = express.Router()
+const router = express.Router();
 
 // add
-router.post("/", verifyAdmin, addProduct )
+router.post("/", verifyAdmin, addProduct);
 
 // update
-router.put("/:prodid", verifyAdmin,  updateProduct)
+router.put("/:prodid", verifyAdmin, updateProduct);
 
 // delete
-router.delete("/:prodid", verifyAdmin,  deleteProduct)
-
+router.delete("/:prodid", verifyAdmin, deleteProduct);
 
 // get one
-router.get("/:prodid", getProduct )
+router.get("/:prodid", getProduct);
 
 // getall
-router.get("/find/all", getallProduct )
+router.get("/find/all", getallProduct);
 
 // search
-router.get("/search/find", searchProduct )
+router.get("/search/find", searchProduct);
 // get by category
-router.get("/category/:category", getallOnCartegory )
+router.get("/category/:category", getallOnCartegory);
 
-
-
-
-
-
-
-export default router
+export default router;
