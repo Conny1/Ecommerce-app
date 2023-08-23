8161dc9a221737f7e8e9499be5cbc83e7f32c691
Conny1 <joelconrad277@gmail.com>
2023-08-23 16:09:49 +0300

    payment

--- api/routers/payments.js
+++ api/routers/payments.js
@@ -1,8 +1,5 @@
 import express from "express";
-import { getmpesaToken } from "../controlers/payment.js";
 
 const router = express.Router();
-
-router.get("/mpesaToken", getmpesaToken);
 
 export default router;
